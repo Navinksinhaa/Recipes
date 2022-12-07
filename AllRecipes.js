37b449b2394a7c660c6999887faf9d1c39c8b487
Navin Kumar <111299104+Navinksinhaa@users.noreply.github.com>
2022-12-07 13:15:54 +0530

    Update AllRecipes.js

--- AllRecipes.js
+++ AllRecipes.js
@@ -5,7 +5,6 @@ import React, { useEffect } from 'react';
 import FavoriteButton from "../../components/FavoriteButton";
 import Recipe from "../../components/Recipe";
 
-const favoriteIconURL = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/favorite.svg'
 
 export const AllRecipes = (props) => {
   
@@ -26,7 +25,7 @@ export const AllRecipes = (props) => {
         <Recipe recipe={recipe} key={recipe.id}>
           <FavoriteButton
             onClickHandler={() => onAddRecipeHandler(recipe)}
-            icon={favoriteIconURL}
+            
           >
             Add to Favorites
           </FavoriteButton>
